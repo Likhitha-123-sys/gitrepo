baa3a745675b3d0632f7b0d62f62b4a3094aaf09
Likhitha-123-sys <adigoppulalikhitha6@gmail.com>
2025-01-10 10:42:30 +0530

    initial

---
+++ app.js
@@ -0,0 +1,39 @@
+// let arr=[1,2,3,4,5,6,7,8,9,10]
+// let even=arr.filter(function(n)
+// {
+//     return n%2==0;
+// });
+// console.log(even);
+// let arr=[1,2,3,4,5,6,7,8,9,10]
+// let even=arr.filter((n)=>(n%2));
+// console.log(even);
+
+// let arr=[1,2,3,4,5,6,7,8,9,10]
+// let even=arr.filter((n)=>!(n%3));
+// console.log(even);
+
+// let arr=[1,2,3,4,5,6,7,8,9,10]
+// let square=arr.map(n=>n*n);
+// console.log(square);
+
+// function Person(name,age)
+// {
+//     this.name=name;
+//     this.age=age;
+//     this.greet=()=>
+//     {
+//         console.log(`Hello ${name}`)
+//     }
+// }
+// const p1=new Person("John",30)
+// console.log(p1.name,p1.age);
+// p1.greet();
+
+// class Student extends Person{
+//     constructor(name){
+//         console.log("creating child class");
+//         super(name);
+//         console.log(`Hello ${name}`)
+//     }
+// }
+
